0f699071a53beebc6cfda65024d4e37b2a92c089
Allen Tan <d4rkmage80@gmail.com>
2020-04-19 23:42:22 -0400

    1. adding in some plinko stuff i forgot to add in before

--- plinko/score.js
+++ plinko/score.js
@@ -1,13 +1,12 @@
 const outputs = [];
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const testSetSize = 10;
-  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  const testSetSize = 400;
+  const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize);
 
   // let numCorrect = 0;
   // for (let i=0; i<testSet.length; i++) {
@@ -20,18 +19,25 @@ function runAnalysis() {
 
   //console.log('accuracy: ', numCorrect/testSetSize);
 
-  const accuracy = _.chain(testSet)
-    .filter(testPoint =>  knn(trainingSet, testPoint[0]) === testPoint[3])
+  _.range(1, 20).forEach(k => {
+    const accuracy = _.chain(testSet)
+    .filter(testPoint =>  knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
     .size()
     .divide(testSetSize)
     .value();
 
-  console.log('accuracy is ', accuracy);
+    console.log(`For a k of ${k} accuracy is ${accuracy}`);
+  })
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
   return  _.chain(data)
-  .map(row => [distance(row[0], point), row[3]])
+  .map(row => {
+    return [
+      distance(_.initial(row), point),
+      _.last(row)
+    ]
+  })
   .sortBy(row => row[0])
   .slice(0, k)
   .countBy(row => row[1])
@@ -44,7 +50,11 @@ function knn(data, point) {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a,b]) => (a-b)**2)
+    .sum()
+    .value()**0.5;
 }
 
 function splitDataset(data, testCount) {
@@ -54,4 +64,21 @@ function splitDataset(data, testCount) {
   const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i=0; i<featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j=0; j<clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 }
